a2b3e290e642f4039341459dc033d3eb53474f84
Rafat <rafatansari12feb1999@gmail.com>
2022-10-12 03:24:39 +0530

    Using library locally

--- app/src/main/java/com/example/logutilexample/MainActivity.kt
+++ app/src/main/java/com/example/logutilexample/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.logutilexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.logutil.Logger
 
 class MainActivity : AppCompatActivity() {
+    val logger = Logger()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logger.debug("This is working")
+
     }
 }
